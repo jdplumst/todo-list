8b0c39235a08ab6ef4911a32afbd9e7e7aae6f12
Jeremiah Plumstead <jeremiah.plumstead@gmail.com>
2022-12-16 01:33:13 -0500

    Add alert when user tries to add mutliple projects with same title

--- src/controller.js
+++ src/controller.js
@@ -8,10 +8,15 @@ export default class Controller {
         // Create new project when new project button is clicked
         this.view.addProjectBtn.addEventListener('click', () => {
             let projectTitle = this.view.getElement('input').value;
-            if (projectTitle === '') return; // SHould also add error message if projectTitle exists already in array
+            if (projectTitle === '') return;
+            if (this.projectExists(projectTitle)) {
+                alert('That project title already exists!');
+                return;
+            }
             this.view.displayNewProject(projectTitle, 'option');
             let project = new Project(projectTitle);
             this.projects.push(project);
+            console.log(this.projects);
         });
 
         // Delete currently displayed project when delete project button clicked
@@ -22,5 +27,13 @@ export default class Controller {
             this.view.deleteElement(`option.${CSS.escape(classProjectTitle)}`);
             this.projects = this.projects.filter(project => project.title !== projectTitle);
         });
+    };
+
+    // Returns true if there already exists a project with a specific title
+    projectExists(projectTitle) {
+        for (let i = 0; i < this.projects.length; i++) {
+            if (this.projects[i].title === projectTitle) return true;
+        }
+        return false;
     };
 };
